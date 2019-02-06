fc41546ee1e567d95d2f979f0d8c478f8feb78de
Michał Pasternakiewicz <czolgistamichal@gmail.com>
2019-02-06 21:09:54 +0100

    New logo added to the login screen. Small gui changes.

--- app/src/main/java/com/example/projekt/car/fragments/HelloFragment.java
+++ app/src/main/java/com/example/projekt/car/fragments/HelloFragment.java
@@ -38,8 +38,8 @@ public class HelloFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.hello_fragment, container, false);
 
-        ImageView imageView = view.findViewById(R.id.imageView2);
-        imageView.setImageResource(R.drawable.sample_face);
+/*        ImageView imageView = view.findViewById(R.id.imageView2);
+        imageView.setImageResource(R.drawable.sample_face);*/
 
        /* TextView textView = view.findViewById(R.id.textView);
         PeopleDataBase peopleDataBase = new PeopleDataBase();
